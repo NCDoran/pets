a4ccc3c4dbbffd1f33c658cd9e67b96a3e3baaf2
Nicole Doran <ncdoran@uw.edu>
2023-02-24 11:59:26 -0800

    testing pets package

--- R/cats.R
+++ R/cats.R
@@ -1,3 +1,11 @@
+#' Expresses your opinion about cats
+#'
+#' @param love A logical argument indicating whether or not you love cats (default = `TRUE`)
+#'
+#' @return One of two possible character strings (`"I love cats!"` or `"I am not a cat person."`).
+#' @export
+#'
+#' @examples cats(TRUE)
 cats <- function(love = TRUE) {
   if(love == TRUE) {
     msg <- "I love cats!"
@@ -11,4 +19,3 @@ cats()
 cats(TRUE)
 cats(FALSE)
 cats("meh")
-check()
